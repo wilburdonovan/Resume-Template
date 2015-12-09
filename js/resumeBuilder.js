9519ba048c0e305a563644a68d707b2fa1e03417
Wilbur Donovan <wilbur.donovan91@hotmail.com>
2015-12-09 17:28:42 +1000

    End lesson 1- Add objects for work, education, projects and bio. Object tested in JSONlint.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,70 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// THE ME OBJECT
+
+var me = {
+  "firstName": "Wilbur",
+  "lastName": "Donovan",
+  "age": 24,
+  "contact": {
+    "email": "wilbur.donovan91@hotmail.com",
+    "mobile": "Australian: 0468999862"
+  },
+  "role": "Computer Science Student",
+  "skills": ["Front End Web Development", "Object-oriented programming", "Software Development", "Business Analysis"],
+  "greeting": "Welcome to my portfolio. Please feel free to have a look at my work.",
+  "myPic": "images/my_picture"
+};
+
+ var education = [
+    {
+      "qualification": "Bachelor of IT (Applied Computer Science)",
+      "institution": "University of Southern Qld",
+      "completion": "In progress, due end 2017"
+    },
+    {
+      "qualification": "Bachelor of  Business (Accounting)",
+      "institution": "Swinburne University of Technology",
+      "completion": "2015"
+    },
+    {
+      "qualification": "Diploma of Project Management",
+      "institution": "Open Training & Education Network",
+      "completion": "2012"
+    },
+    {
+      "qualification": "Front-End Web Development Nanodegree",
+      "institution": "Udacity",
+      "completion": "2016"
+    }
+  ];
+
+var work = [
+  {
+    "position": "Fitness Coach",
+    "employer": "Self-employed",
+    "dates": "2011-2015"
+  },
+  {
+    "position": "Manager/ Crew Member",
+    "employer": "McDonalds",
+    "dates": "2005-2009"
+  }
+];
+
+var projects = [
+  {
+    "name": "Fake1",
+    "link": "github.com/fake1",
+    "description": "fake project 1",
+    "category": "Business Analysis"
+  },
+  {
+    "name": "Fake2",
+    "link": "github.com/fake2",
+    "description": "fake project 2",
+    "category": "Business Analysis"
+  }
+];
+
+
+
+
